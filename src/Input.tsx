635844db5c7979c7b1a0511109b41a5ff6b849d3
Stephen Vector <stephenvector@pm.me>
2019-09-26 15:35:48 -0400

    Configuring test coverage.

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,6 +1,21 @@
+import React from "react";
 import styled from "@emotion/styled";
+import Label from "./Label";
 
-export default styled("input")`
+const InputControl = styled("input")`
   font: inherit;
   padding: 0.5rem;
 `;
+
+interface Props {
+  label: string;
+}
+
+export default function Input({ label }: Props) {
+  return (
+    <div>
+      <Label>{label}</Label>
+      <InputControl />
+    </div>
+  );
+}
--- src/__tests__/Button.test.tsx
+++ src/__tests__/Button.test.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { mount } from "enzyme";
-import Button from "./Button";
+import { Button } from "../";
 
 describe("<Button />", () => {
   it("Should render child text", () => {
     const wrapper = mount(<Button>Hello</Button>);
-    console.log(wrapper.find("button"));
-    console.log("000", wrapper.text(), "000");
     expect(wrapper.text()).toEqual("Hello");
   });
 });
