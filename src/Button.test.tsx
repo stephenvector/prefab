3e82a492fe2fa133066c98450f640a793016d944
Stephen Vector <stephenvector@pm.me>
2019-09-26 14:52:50 -0400

    Configuring jest and enzyme.

--- src/Button.test.tsx
+++ src/Button.test.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { shallow } from "enzyme";
-import { Button } from "./";
+import { mount } from "enzyme";
+import Button from "./Button";
 
 describe("<Button />", () => {
   it("Should render child text", () => {
-    const wrapper = shallow(<Button>Hello</Button>);
-    expect(wrapper.text()).toBe("Hello");
+    const wrapper = mount(<Button>Hello</Button>);
+    console.log(wrapper.find("button"));
+    console.log("000", wrapper.text(), "000");
+    expect(wrapper.text()).toEqual("Hello");
   });
 });
