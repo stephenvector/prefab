08ce76bee89b98c9be43efadccac21d879d64010
Stephen Vector <stephenvector@pm.me>
2019-10-06 20:30:33 -0400

    Organizing stories into their own directory.

---
+++ src/stories/ColorPicker.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { ColorPicker } from "../";
+
+storiesOf("ColorPicker").add("default", () => <ColorPicker />);
---
+++ src/stories/DatePicker.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { DatePicker } from "../";
+
+storiesOf("DatePicker").add("default", () => <DatePicker />);
