87f664c2e3b4515b53194bab065277dfabccf8d4
Stephen Vector <stephenvector@pm.me>
2019-10-06 20:32:47 -0400

    Updating styles to new aphrodite setup.

--- src/DatePicker.tsx
+++ src/DatePicker.tsx
@@ -1,67 +1,31 @@
 import React, { useState } from "react";
-// import { PrefabThemeConfig, lightTheme } from "./themes";
+import { css, StyleSheet } from "aphrodite";
 
-// const Wrapper = styled.div`
-//   background: ${p => p.theme.colors.background};
-//   width: 21rem;
-//   border: 1px solid
-//     ${(p: { theme: PrefabThemeConfig }) => p.theme.colors.border};
-//   border-radius: ${p => p.theme.sizing.borderRadius};
-// `;
-
-// Wrapper.defaultProps = {
-//   theme: lightTheme
-// };
-
-// const Header = styled.div<{ theme: PrefabThemeConfig }>`
-//   display: flex;
-//   justify-content: space-between;
-//   line-height: 3rem;
-//   border-bottom: 1px solid
-//     ${(p: { theme: PrefabThemeConfig }) => p.theme.colors.border};
-// `;
-
-// Header.defaultProps = {
-//   theme: lightTheme
-// };
-
-// const DayBox = styled.button<{
-//   theme: PrefabThemeConfig;
-// }>`
-//   background: transparent;
-//   height: 3rem;
-//   width: 3rem;
-//   min-width: 3rem;
-//   border: none;
-//   font: inherit;
-//   user-select: none;
-//   border-radius: ${p => p.theme.sizing.borderRadius};
-//   &:hover {
-//     background: ${p => p.theme.colors.accent};
-//     color: #fff;
-//   }
-// `;
-
-// DayBox.defaultProps = {
-//   theme: lightTheme
-// };
-
-// const NextPreviousButton = styled.button`
-//   background: transparent;
-//   padding: 0;
-//   cursor: pointer;
-//   display: inline-block;
-//   font: inherit;
-//   border: none;
-//   height: 3rem;
-//   line-height: 3rem;
-//   width: 3rem;
-//   text-align: center;
-// `;
-
-// NextPreviousButton.defaultProps = {
-//   theme: lightTheme
-// };
+const datePickerStylesheet = StyleSheet.create({
+  wrapper: {
+    width: "21rem"
+  },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    lineHeight: "3rem"
+  },
+  dayBox: {
+    background: "transparent",
+    border: "none",
+    font: "inherit",
+    height: "3rem",
+    width: "3rem"
+  },
+  nextPreviousButton: {
+    background: "transparent",
+    border: "none",
+    font: "inherit",
+    padding: 0,
+    height: "3rem",
+    lineHeight: "3rem"
+  }
+});
 
 const monthFormatter = new Intl.DateTimeFormat("en-us", { month: "long" });
 
@@ -91,15 +55,23 @@ export default function DatePicker() {
   }
 
   return (
-    <div>
-      <div>
-        <button onClick={previousMonth} type="button">
+    <div className={css(datePickerStylesheet.wrapper)}>
+      <div className={css(datePickerStylesheet.header)}>
+        <button
+          className={css(datePickerStylesheet.nextPreviousButton)}
+          onClick={previousMonth}
+          type="button"
+        >
           &larr;
         </button>
         <div style={{ textAlign: "center" }}>
           {monthFormatter.format(now)} {now.getFullYear()}
         </div>
-        <button onClick={nextMonth} type="button">
+        <button
+          className={css(datePickerStylesheet.nextPreviousButton)}
+          onClick={nextMonth}
+          type="button"
+        >
           &rarr;
         </button>
       </div>
@@ -114,12 +86,14 @@ export default function DatePicker() {
         {Array(startDayOfWeek)
           .fill(0)
           .map((_item, index) => (
-            <button key={index} />
+            <button className={css(datePickerStylesheet.dayBox)} key={index} />
           ))}
         {Array(numDaysInMonth)
           .fill(0)
           .map((_item, index) => (
-            <button key={index}>{index + 1}</button>
+            <button className={css(datePickerStylesheet.dayBox)} key={index}>
+              {index + 1}
+            </button>
           ))}
       </div>
     </div>
