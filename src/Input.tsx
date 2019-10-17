e13bf81dd361d5a7f8e5f267223cc8760de2c677
Stephen Vector <stephenvector@pm.me>
2019-10-16 23:31:51 -0400

    Setting up the bare bones of a progress bar.

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import styled from "styled-components";
 import { Label } from "./";
 
@@ -15,13 +15,31 @@ const InputControl = styled.input`
   font: inherit;
   border: none;
   background: #f2f2f2;
+  border-radius: 3px;
 `;
 
-export default function Input({ label }: InputProps) {
+export default function Input({
+  label,
+  onChange,
+  value,
+  error,
+  errorMessage,
+  ...otherProps
+}: InputProps) {
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      onChange(e.target.value);
+    },
+    [onChange]
+  );
+
   return (
     <>
       {label !== undefined && <Label>{label}</Label>}
-      <InputControl />
+      <InputControl {...otherProps} value={value} onChange={handleChange} />
+      {error !== undefined && errorMessage !== undefined && (
+        <div>ERROR: {errorMessage}</div>
+      )}
     </>
   );
 }
--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -1,50 +1,21 @@
 import React from "react";
-// import { css, StyleSheet } from "aphrodite/no-important";
-// import { lightTheme } from "./themes";
-
-// const progressBarStyles = StyleSheet.create({
-//   wrapper: {
-//     background: `var(--colors-background, ${lightTheme.colors.background})`,
-//     border: "none",
-//     overflow: "hidden",
-//     borderRadius: "0.5rem",
-//     boxShadow: `inset 0 0 0 2px var(--colors-accent, ${lightTheme.colors.accent})`
-//   },
-//   progress: {
-//     background: `var(--colors-accent, ${lightTheme.colors.accent})`,
-//     border: "none",
-//     font: "inherit",
-//     height: "1rem",
-//     borderRadius: "0.5rem",
-//     fontWeight: "bold",
-//     fontSize: "90%",
-//     lineHeight: "1rem",
-//     textAlign: "center",
-//     color: `var(--colors-background, ${lightTheme.colors.background})`
-//   }
-// });
+import styled from "styled-components";
 
 interface Props {
   value: number;
-  showText: boolean;
 }
 
-function ProgressBar({ value, showText }: Props) {
-  // const widthStyle = StyleSheet.create({
-  //   width: {
-  //     width: `${value}%`
-  //   }
-  // });
-
+function ProgressBar({ value }: Props) {
   return (
-    <div>
-      <div>{showText && `${value}%`}</div>
+    <div
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin={0}
+      aria-valuemax={100}
+    >
+      <div />
     </div>
   );
 }
-
-ProgressBar.defaultProps = {
-  showText: false
-};
 
 export default ProgressBar;
