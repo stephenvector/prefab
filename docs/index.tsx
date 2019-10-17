3821534a5616a3a226ebdffb1b19c3f6b6319aec
Stephen Vector <stephenvector@pm.me>
2019-10-16 23:36:37 -0400

    Adding more examples and testing out the components in more contexts.

--- docs/index.tsx
+++ docs/index.tsx
@@ -12,34 +12,59 @@ import {
   DatePicker,
   PrefabThemeProvider,
   Slider,
+  Hr,
   Textarea,
   Display1,
+  Display2,
+  Display3,
+  Display4,
+  H1,
+  H2,
   H3,
-  PrefabTheme,
+  H4,
+  H5,
+  H6,
+  defaultPrefabTheme,
   Select,
-  Paragraph
+  Paragraph,
+  Padding
 } from "../src";
 
 import CodeSnippet from "./CodeSnippet";
 import DemoController from "./DemoController";
 
-const buttonCode = `<Button>Button</Button>`;
+const DOCS_SELECT_OPTIONS = [
+  {
+    label: "First Item",
+    value: "first"
+  },
+  {
+    label: "Second Item",
+    value: "first"
+  },
+  {
+    label: "Third Item",
+    value: "first"
+  }
+];
 
 function Docs() {
   return (
-    <PrefabThemeProvider theme={PrefabTheme}>
+    <PrefabThemeProvider theme={DefaultPrefabTheme}>
       <div className="Docs">
         <header>
           <Container>
             <Row>
               <Column>
-                <Display1>prefab</Display1>
-                <Paragraph>A set of themeable UI React components.</Paragraph>
-                <Paragraph>
-                  <a href="https://github.com/stephenvector/prefab">
-                    View source on GitHub
-                  </a>
-                </Paragraph>
+                <Padding top={12}>
+                  <Display1>prefab</Display1>
+                  <Paragraph>A set of themeable UI React components.</Paragraph>
+                  <Paragraph>
+                    <a href="https://github.com/stephenvector/prefab">
+                      View source on GitHub
+                    </a>
+                  </Paragraph>
+                </Padding>
               </Column>
             </Row>
           </Container>
@@ -48,13 +73,22 @@ function Docs() {
           <Container>
             <Row>
               <Column>
-                <h2>Components</h2>
-
-                <CodeSnippet code={buttonCode} />
-
-                <H3>Button</H3>
-                <Button>Button</Button>
-
+                <Box aspectRatio={2 / 4} centerContent>
+                  <Button>Button</Button>
+                  <CodeSnippet code={`<Button>Normal Button</Button>`} />
+                </Box>
+              </Column>
+              <Column>
+                <Box aspectRatio={1} centerContent>
+                  <Button fullWidth>Button</Button>
+                  <CodeSnippet
+                    code={`<Button fullWidth>Full Width Button</Button>`}
+                  />
+                </Box>
+              </Column>
+            </Row>
+            <Row>
+              <Column>
                 <h3>Button Group</h3>
                 <ButtonGroup>
                   <Button>Button</Button>
@@ -67,16 +101,44 @@ function Docs() {
                   component={ColorPicker}
                 />
 
+                <div id="typography">
+                  <h2>Typography</h2>
+                  <Display1>Display1</Display1>
+                  <Display2>Display2</Display2>
+                  <Display3>Display3</Display3>
+                  <Display4>Display4</Display4>
+                  <H1>Heading 1</H1>
+                  <H2>Heading 2</H2>
+                  <H3>Heading 3</H3>
+                  <H4>Heading 4</H4>
+                  <H5>Heading 5</H5>
+                  <H6>Heading 6</H6>
+                </div>
+
+                <Hr />
+
                 <H3>Carousel</H3>
                 <Carousel>
-                  <Box centerContent aspectRatio={9 / 16}>
-                    Slide One
+                  <Box
+                    style={{ background: "#777", color: "#fff" }}
+                    centerContent
+                    aspectRatio={9 / 16}
+                  >
+                    <H4>Slide One</H4>
                   </Box>
-                  <Box centerContent aspectRatio={9 / 16}>
-                    Slide Two
+                  <Box
+                    style={{ background: "#444", color: "#fff" }}
+                    centerContent
+                    aspectRatio={9 / 16}
+                  >
+                    <H4>Slide Two</H4>
                   </Box>
-                  <Box centerContent aspectRatio={9 / 16}>
-                    Slide Three
+                  <Box
+                    style={{ background: "#555", color: "#fff" }}
+                    centerContent
+                    aspectRatio={9 / 16}
+                  >
+                    <H4>Slide Three</H4>
                   </Box>
                 </Carousel>
 
@@ -104,7 +166,7 @@ function Docs() {
             <Row>
               <Column>
                 <div>
-                  <Select options={[]} value={undefined} />
+                  <Select options={DOCS_SELECT_OPTIONS} value={undefined} />
                 </div>
               </Column>
               <Column>
