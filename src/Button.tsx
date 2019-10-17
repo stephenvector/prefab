1b415e8c62077854daf6138b75fe04c97c4575aa
Stephen Vector <stephenvector@pm.me>
2019-10-16 23:23:20 -0400

    Fixing the button theme and test issues.

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,24 +1,56 @@
-import styled from "styled-components";
+import React from "react";
+import styled, { DefaultTheme } from "styled-components";
+import { lightTheme, lightenHex } from "./";
 
-type ButtonProps = {
-  fullWidth?: true;
-};
+// const Button = function Button(props: any) {
+//   return <button {...props} />;
+// };
+
+interface ButtonProps {
+  fullWidth: boolean;
+  theme: DefaultTheme;
+}
 
 const Button = styled.button<ButtonProps>`
   font: inherit;
   font-size: 1rem;
+  font-weight: 700;
   margin: 0;
   padding: 0 2rem;
-  line-height: 3rem;
+  cursor: pointer;
+
   display: inline-block;
   border: none;
-  background: blue;
-  color: #fff;
-  width: ${p => (p.fullWidth === true ? "100%" : "auto")};
-  :hover {
-    cursor: pointer;
-    background: blue;
+  :focus {
+    outline: solid 6px #ccf;
   }
+  ${props => {
+    const { theme, fullWidth } = Object.assign(
+      {},
+      {
+        theme: lightTheme,
+        fullWidth: false
+      },
+      props
+    );
+    return {
+      lineHeight:
+        theme.sizing.formControls !== undefined
+          ? theme.sizing.formControls
+          : lightTheme.sizing.formControls,
+      width: fullWidth ? "100%" : "auto",
+      borderRadius: theme.sizing.borderRadius,
+      color: theme.colors.bg,
+      ":hover": {
+        background: lightenHex(theme.colors.accent, 0.3)
+      }
+    };
+  }}
 `;
+
+Button.defaultProps = {
+  theme: lightTheme,
+  fullWidth: false
+};
 
 export default Button;
