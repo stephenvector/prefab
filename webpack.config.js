1e05ed3bb2c4cf724bf49369d5016e0103c2d9fa
Stephen Vector <stephenvector@pm.me>
2019-10-17 09:51:04 -0400

    Switching to production mode for smaller docs builds.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   watch: false,
   entry: "./docs/index.tsx",
   output: {
