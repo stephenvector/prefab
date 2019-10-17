d7432d5a71e05a5c0f5999b8877806c7a653fe94
Stephen Vector <stephenvector@pm.me>
2019-10-17 09:49:07 -0400

    Adding outline support and custom override colors.

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,56 +1,70 @@
 import React from "react";
 import styled, { DefaultTheme } from "styled-components";
-import { lightTheme, lightenHex } from "./";
-
-// const Button = function Button(props: any) {
-//   return <button {...props} />;
-// };
+import { defaultPrefabTheme, lightenHex } from "./";
 
 interface ButtonProps {
   fullWidth?: boolean;
-  theme?: DefaultTheme;
+  theme: DefaultTheme;
+  outline?: boolean;
+  bg?: string;
+  fg?: string;
 }
 
-const Button = styled.button<ButtonProps>`
+interface ButtonPropsComputed extends ButtonProps {
+  bg: string;
+  fg: string;
+  borderColor: string;
+}
+
+const Button = styled.button.attrs((p: ButtonProps) => {
+  let bg = p.bg ? p.bg : p.theme.colors.accent;
+  let fg = p.fg ? p.fg : p.theme.colors.bg;
+  let borderColor = p.outline ? fg : bg;
+  if (p.outline) {
+    const temp = bg;
+    bg = fg;
+    fg = temp;
+  }
+  return {
+    bg,
+    fg,
+    borderColor
+  };
+})<ButtonPropsComputed>`
   font: inherit;
   font-size: 1rem;
   font-weight: 700;
   margin: 0;
   padding: 0 2rem;
   cursor: pointer;
-
   display: inline-block;
   border: none;
+  width: ${p => (p.fullWidth ? "100%" : "auto")};
+  line-height: ${p => p.theme.sizing.formControls};
+  border-radius: ${p => p.theme.sizing.borderRadius};
+  background: ${p => p.bg};
+  color: ${p => p.fg};
+  box-shadow: ${p =>
+    `inset 0 0 0 ${p.theme.sizing.border} ${p.theme.colors.accent}`};
   :focus {
     outline: solid 6px #ccf;
+    outline-offset: 2px;
   }
-  ${props => {
-    const { theme, fullWidth } = Object.assign(
-      {},
-      {
-        theme: lightTheme,
-        fullWidth: false
-      },
-      props
-    );
-    return {
-      lineHeight:
-        theme.sizing.formControls !== undefined
-          ? theme.sizing.formControls
-          : lightTheme.sizing.formControls,
-      width: fullWidth ? "100%" : "auto",
-      borderRadius: theme.sizing.borderRadius,
-      color: theme.colors.bg,
-      ":hover": {
-        background: lightenHex(theme.colors.accent, 0.3)
-      }
-    };
-  }}
+  :hover {
+    color: ${p => (p.outline ? p.bg : p.fg)};
+    background: ${({ theme }) => lightenHex(theme.colors.accent, 0.3)};
+    box-shadow: ${({ theme }) =>
+      `inset 0 0 ${theme.sizing.border} 0 ${lightenHex(
+        theme.colors.accent,
+        0.3
+      )}`};
+  }
 `;
 
 Button.defaultProps = {
-  theme: lightTheme,
-  fullWidth: false
+  theme: defaultPrefabTheme,
+  fullWidth: false,
+  outline: false
 };
 
 export default Button;
