aa1246ae282598de93a2d1ac3ad5ba1b9a684d22
Stephen Vector <stephenvector@pm.me>
2019-10-20 18:38:35 -0400

    Added random id generating function.

--- src/utils.tsx
+++ src/utils.tsx
@@ -170,3 +170,29 @@ export function darkenHex(hex: string, amount: number) {
 
   return getHexFromRGB(newColor);
 }
+
+export function getRandomID(
+  existingIDs: string[] = [],
+  length: number = 8,
+  characterSet: string = "abcdefghijklmnopqrstuvwxyz"
+): string {
+  const numPossibilities = Math.pow(characterSet.length, length);
+
+  if (existingIDs.length === numPossibilities) {
+    throw new Error(
+      "Possible combinations exhausted! Need larget character set."
+    );
+  }
+
+  let randomID = "";
+
+  for (let i = 0; i < length; i++) {
+    randomID += characterSet[Math.floor(Math.random() * characterSet.length)];
+  }
+
+  if (existingIDs.indexOf(randomID) !== -1) {
+    return getRandomID(existingIDs, length);
+  }
+
+  return randomID;
+}
