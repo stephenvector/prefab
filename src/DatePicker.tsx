d2f9edfd834d80ed8290d1fea42744b7f76c2030
Stephen Vector <stephenvector@pm.me>
2019-10-21 08:08:15 -0400

    Integrating theme styling.

--- src/DatePicker.tsx
+++ src/DatePicker.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
 import { ArrowLeft, ArrowRight } from "@stephenvector/picto";
 
 const Wrapper = styled.div`
   width: calc(3rem * 7);
+  border-radius: ${p => p.theme.sizing.borderRadius};
+  box-shadow: inset 0 0 ${p => p.theme.sizing.border} 0
+    ${p => p.theme.colors.meta};
 `;
 
 const NextPrevMonthButton = styled.button`
@@ -13,6 +16,16 @@ const NextPrevMonthButton = styled.button`
   border: none;
   width: 3rem;
   height: 3rem;
+  background: transparent;
+  cursor: pointer;
+  border-radius: ${p => p.theme.sizing.borderRadius};
+  :hover {
+    background: ${p => p.theme.colors.accent};
+    color: ${p => p.theme.colors.bg};
+    svg polyline {
+      stroke: #fff;
+    }
+  }
 `;
 
 const DatePickerHeader = styled.div`
@@ -21,14 +34,17 @@ const DatePickerHeader = styled.div`
   line-height: 3rem;
 `;
 
-const DayButton = styled.button`
+const DayButton = styled.button<{ isSelected: boolean }>`
   width: 3rem;
   height: 3rem;
   border: none;
   font: inherit;
-  background: transparent;
+  background: ${p => (p.isSelected ? p.theme.colors.accent : "transparent")};
+  color: ${p => (p.isSelected ? p.theme.colors.bg : "inherit")};
+  cursor: pointer;
+  border-radius: ${p => p.theme.sizing.borderRadius};
   :hover {
-    background: blue;
+    background: ${p => p.theme.colors.accent};
     color: #fff;
   }
 `;
@@ -40,8 +56,36 @@ const BlankDay = styled.span`
 
 const monthFormatter = new Intl.DateTimeFormat("en-us", { month: "long" });
 
-export default function DatePicker() {
-  const [now, setNow] = useState(new Date(Date.now()));
+type DatePickerProps = {
+  value: number;
+  onChange(val: number): void;
+};
+
+type YearMonthDate = {
+  year: number;
+  month: number;
+  date: number;
+};
+
+export default function DatePicker({ value, onChange }: DatePickerProps) {
+  const [now, setNow] = useState(new Date(value));
+
+  const [currentSelectedYearMonthDay, setCurrentSelected] = useState<
+    undefined | YearMonthDate
+  >(undefined);
+
+  useEffect(() => {
+    const newNow = new Date(value);
+    setNow(newNow);
+  }, [value]);
+
+  useEffect(() => {
+    setCurrentSelected({
+      year: now.getFullYear(),
+      month: now.getMonth(),
+      date: now.getDate()
+    });
+  }, [now]);
 
   const numDaysInMonth = new Date(
     now.getFullYear(),
@@ -54,6 +98,18 @@ export default function DatePicker() {
     now.getMonth(),
     1
   ).getDay();
+
+  const handleSelectDate = useCallback(
+    (date: number) => {
+      const newValue = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        date
+      ).getTime();
+      onChange(newValue);
+    },
+    [now, onChange]
+  );
 
   function previousMonth() {
     const newNow = new Date(now.getFullYear(), now.getMonth() - 1);
@@ -87,9 +143,20 @@ export default function DatePicker() {
           ))}
         {Array(numDaysInMonth)
           .fill(0)
-          .map((_item, index) => (
-            <DayButton key={index}>{index + 1}</DayButton>
-          ))}
+          .map((_item, index) => {
+            const isSelected = false;
+            return (
+              <DayButton
+                onClick={() => {
+                  handleSelectDate(index);
+                }}
+                key={index}
+                isSelected={isSelected}
+              >
+                {index + 1}
+              </DayButton>
+            );
+          })}
       </div>
     </Wrapper>
   );
