45bc0f3a0b2bdf3eaa99ea4653520870e8fffb9f
Stephen Vector <stephenvector@pm.me>
2019-10-24 10:35:06 -0400

    Making responsive.

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,12 +1,62 @@
 import styled from "styled-components";
+import { defaultPrefabTheme } from "./";
 
-const Column = styled.div`
+type ColumnProps = {
+  xs?: string;
+  sm?: string;
+  md?: string;
+  lg?: string;
+};
+
+const Column = styled.div<ColumnProps>`
   flex: 0 0 auto;
   flex-grow: 1;
   flex-basis: 0;
-  padding: 1rem;
+  min-height: 1px;
+  display: block;
+  padding-left: 1rem;
+  padding-right: 1rem;
   box-sizing: border-box;
   max-width: 100%;
+  ${({ xs }) => {
+    if (!xs) return;
+    return {
+      flexBasis: xs,
+      maxWidth: xs
+    };
+  }}
+        
+  @media screen and (min-width: ${p => p.theme.breakpoints.sm}) {
+    ${({ sm }) => {
+      if (!sm) return;
+      return {
+        flexBasis: sm,
+        maxWidth: sm
+      };
+    }}
+  }
+  @media screen and (min-width: ${p => p.theme.breakpoints.md}) {
+    ${({ md }) => {
+      if (!md) return;
+      return {
+        flexBasis: md,
+        maxWidth: md
+      };
+    }}
+  }
+  @media screen and (min-width: ${p => p.theme.breakpoints.lg}) {
+    ${({ lg }) => {
+      if (!lg) return;
+      return {
+        flexBasis: lg,
+        maxWidth: lg
+      };
+    }}
+  }
 `;
+
+Column.defaultProps = {
+  theme: defaultPrefabTheme
+};
 
 export default Column;
