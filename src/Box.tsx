96dbe8f2a85632ad40dd0e70f90e9f94cb139662
Stephen Vector <stephenvector@pm.me>
2019-10-24 10:34:44 -0400

    Adding bg, fg, margin, & padding support.

--- src/Box.tsx
+++ src/Box.tsx
@@ -2,13 +2,120 @@ import React from "react";
 import styled from "styled-components";
 
 type BoxProps = React.HTMLAttributes<HTMLDivElement> & {
+  bg?: string;
+  fg?: string;
   aspectRatio?: number;
   centerContent?: boolean;
+  paddingTop?: number;
+  paddingLeft?: number;
+  paddingRight?: number;
+  paddingBottom?: number;
+  paddingX?: number;
+  paddingY?: number;
+  padding?: number;
+  marginTop?: number;
+  marginleft?: number;
+  marginRight?: number;
+  marginBottom?: number;
+  marginX?: number;
+  marginY?: number;
+  margin?: number;
 };
 
 const BoxInner = styled.div``;
 
 const BoxOuter = styled.div<BoxProps>`
+  ${({
+    bg,
+    fg,
+    paddingTop,
+    paddingLeft,
+    paddingRight,
+    paddingBottom,
+    paddingX,
+    paddingY,
+    padding,
+    marginTop,
+    marginleft,
+    marginRight,
+    marginBottom,
+    marginX,
+    marginY,
+    margin
+  }) => {
+    const rules: { [key: string]: any } = {};
+
+    if (bg) {
+      rules.background = bg;
+    }
+
+    if (fg) {
+      rules.color = fg;
+    }
+
+    if (paddingTop) {
+      rules.paddingTop = `${paddingTop}rem`;
+    }
+
+    if (paddingLeft) {
+      rules.paddingLeft = `${paddingLeft}rem`;
+    }
+
+    if (paddingRight) {
+      rules.paddingRight = `${paddingRight}rem`;
+    }
+
+    if (paddingBottom) {
+      rules.paddingBottom = `${paddingBottom}rem`;
+    }
+
+    if (paddingX) {
+      rules.paddingLeft = `${paddingX}rem`;
+      rules.paddingRight = `${paddingX}rem`;
+    }
+
+    if (paddingY) {
+      rules.paddingTop = `${paddingY}rem`;
+      rules.paddingBottom = `${paddingY}rem`;
+    }
+
+    if (padding) {
+      rules.padding = `${padding}rem`;
+    }
+
+    if (marginTop) {
+      rules.marginTop = `${marginTop}rem`;
+    }
+
+    if (marginleft) {
+      rules.marginLeft = `${marginleft}rem`;
+    }
+
+    if (marginRight) {
+      rules.marginRight = `${marginRight}rem`;
+    }
+
+    if (marginBottom) {
+      rules.marginBottom = `${marginBottom}rem`;
+    }
+
+    if (marginX) {
+      rules.marginLeft = `${marginX}rem`;
+      rules.marginRight = `${marginX}rem`;
+    }
+
+    if (marginY) {
+      rules.marginTop = `${marginY}rem`;
+      rules.marginBottom = `${marginY}rem`;
+    }
+
+    if (margin) {
+      rules.margin = `${margin}rem`;
+    }
+
+    return rules;
+  }}
+
   ${p => {
     if (p.aspectRatio === undefined) {
       return {};
