39835818011b5bea3f50bc3b9c3d4bdb9ea5a7d9
Stephen Vector <stephenvector@pm.me>
2019-11-03 19:25:44 -0500

    Fixing display sizes, weight & heading margins.

--- src/Typography.tsx
+++ src/Typography.tsx
@@ -3,8 +3,7 @@ import styled from "./styled";
 export const Display1 = styled.div`
   line-height: 1.1;
   font-size: 8rem;
-  font-weight: 900;
-  letter-spacing: -0.05em;
+  font-weight: 700;
 `;
 
 export const Display2 = styled.div`
@@ -21,44 +20,50 @@ export const Display3 = styled.div`
 
 export const Display4 = styled.div`
   line-height: 1.1;
-  font-size: 3rem;
-  font-weight: 300;
+  font-size: 4rem;
+  font-weight: 700;
 `;
 
 export const H1 = styled.h1`
   line-height: 1.1;
   font-size: 3rem;
   font-weight: 700;
+  margin: 1.67rem 0;
 `;
 
 export const H2 = styled.h2`
   line-height: 1.1;
   font-size: 2.4rem;
   font-weight: 700;
+  margin: 1.67rem 0;
 `;
 
 export const H3 = styled.h3`
   line-height: 1.1;
   font-size: 2rem;
   font-weight: 700;
+  margin: 1.67rem 0;
 `;
 
 export const H4 = styled.h4`
   line-height: 1.1;
   font-size: 1.6rem;
   font-weight: 700;
+  margin: 1.67rem 0;
 `;
 
 export const H5 = styled.h5`
   line-height: 1.1;
   font-size: 1.3rem;
   font-weight: 700;
+  margin: 1.67rem 0;
 `;
 
 export const H6 = styled.h6`
   line-height: 1.1;
   font-size: 1.1rem;
   font-weight: 700;
+  margin: 1.67rem 0;
 `;
 
 export const Paragraph = styled.p`
