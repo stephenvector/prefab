2e2e0fa0b8d8c63d7c4023bdd83ece68cb7245d9
Stephen Vector <stephenvector@pm.me>
2019-11-03 19:24:29 -0500

    Fixing button.

--- src/Button.tsx
+++ src/Button.tsx
@@ -12,17 +12,19 @@ interface ButtonProps {
 const Button = styled.button<ButtonProps>(props => {
   let bg = props.bg ? props.bg : props.theme.colors.accent;
   let fg = props.fg ? props.fg : props.theme.colors.bg;
-  let borderColor = props.outline ? fg : bg;
+
   if (props.outline) {
     const temp = bg;
     bg = fg;
     fg = temp;
   }
+  let borderColor = props.outline ? fg : bg;
   return {
     font: "inherit",
     fontSize: "1rem",
     fontWeight: 700,
     margin: 0,
+    textDecoration: "none",
     padding: "0 2rem",
     cursor: "pointer",
     display: "inline-block",
