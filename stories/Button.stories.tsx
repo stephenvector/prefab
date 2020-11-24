2547756055cdb05bd30673212e83b089f546cc35
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2020-11-23 16:45:34 -0800

    Removing old storybook stories.

--- stories/Button.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Button from "../src/Button";
-
-export default {
-  title: "Button"
-};
-
-export const SimpleButton = () => <Button type="button">Button</Button>;
